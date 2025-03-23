9c824cb1b23d4a0ec7939e483a5f91e3c0047b42
tmz0822 <97086526+tmz0822@users.noreply.github.com>
2025-03-23 20:45:53 +0800

    add sql

--- db/populateData.js
+++ db/populateData.js
@@ -1,3 +1,18 @@
 const SQL = `
-  CREATE TABLE
+  CREATE TABLE users (
+    id          INTEGER GENERATE ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name  VARCHAR(100) NOT NULL,
+    last_name   VARCHAR(100) NOT NULL,
+    username    VARCHAR(100) UNIQUE NOT NULL,
+    is_member   BOOLEAN DEFAULT FALSE,
+    is_admin    BOOLEAN DEFAULT FALSE
+  );
+
+  CREATE TABLE messages (
+    id          INTEGER GENERATE ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id     INTEGER NOT NULL REFERENCES users(id),
+    title       VARCHAR(255) NOT NULL,
+    text        TEXT NOT NULL,
+    created_at  TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+  );
 `;
