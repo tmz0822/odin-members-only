667415073f4e05b32064c92793af089b121ffd52
tmz0822 <97086526+tmz0822@users.noreply.github.com>
2025-03-24 13:18:53 +0800

    add auth middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,7 @@
+function isAuthenticated(req, res, next) {}
+
+function isMember(req, res, next) {}
+
+function isAdmin(req, res, next) {}
+
+module.exports = { isAuthenticated, isMember, isAdmin };
